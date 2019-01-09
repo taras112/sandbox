868b6a94e870e09d623c15073ce16fde27c4c061
Taras <t.shkapyak@gmail.com>
2019-01-09 22:59:10 +0200

    .

--- V003__Add employee title relationship.sql
+++ V003__Add employee title relationship.sql
@@ -1,6 +1,7 @@
 ALTER TABLE employees ADD title_id INT NOT NULL;
-
-DECLARE @TempVar VARCHAR(100) = 'aaa'
+GO
+DECLARE @TempVar VARCHAR(100) = 'aaa';
+GO
 ALTER TABLE employees ADD CONSTRAINT employees_titles_title_id 
 	FOREIGN KEY (title_id) 
 	REFERENCES titles (id);
